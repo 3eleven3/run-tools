94ec35eff2aeaaa10701d6ba1ef646f46516de93
3eleven3 <cameron.blair.wu+gh@gmail.com>
2026-03-15 18:08:36 -0400

    forgot the prefix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	// required for github pages
-	// base: "/run-tools/",
+	base: "/run-tools/",
 	server: {
 		host: "0.0.0.0",
 		port: 5000,
